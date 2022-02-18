170df352ad9590cdf6d3e12f3bd2969fba4564ce
Dixon Calderon <dixoncalderon.shopping@gmail.com>
2022-02-17 19:10:55 -0500

    ortega

--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render (request, 'home.html',{'name':'Dixon Calderon'})
+    return render (request, 'home.html',{'name':'Dixon Calderon ortega'})
 
 # Create your views here.
