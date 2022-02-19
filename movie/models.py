be15a4d260b8c4a3d26f2bfb9c0d11d5c7aa389e
Dixon Calderon <dixoncalderon.shopping@gmail.com>
2022-02-19 10:58:15 -0500

    superuser

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    describtion = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(blank=True)
+    
 # Create your models here.
