2fefce192c9f7cdc5c004d8a01db51f354a7c861
Dixon Calderon <dixoncalderon.shopping@gmail.com>
2022-03-03 11:11:53 -0500

    Visualizacion de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
    # return render (request, 'home.html',{'name':'Dixon Andres Calderon ortega'})
     searchTerm = request.GET.get('searchMovie')
-    return render(request, 'home.html', {'searchTerm':searchTerm})
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 # Create your views here.
 
-
+ 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page </h1>')
