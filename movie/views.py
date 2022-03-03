b7ba631da651aff46000905674f197fb3bbbcdf3
Dixon Calderon <dixoncalderon.shopping@gmail.com>
2022-03-03 10:40:36 -0500

    Formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render (request, 'home.html',{'name':'Dixon Andres Calderon ortega'})
-
+   # return render (request, 'home.html',{'name':'Dixon Andres Calderon ortega'})
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
 # Create your views here.
+
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page </h1>')
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -17,12 +17,14 @@ from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
+
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',movieViews.home),
+    path('about/', movieViews.about),
 ]
 
-urlpatterns += static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
